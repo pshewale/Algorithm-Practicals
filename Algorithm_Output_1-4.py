86e8a2c5deec3f4cef79b90c50e92ff15f5d936d
pshewale <77521813+pshewale@users.noreply.github.com>
2021-02-11 21:44:05 +0530

    Algorithm
    Practicals

---
+++ Algorithm_Output_1-4.py
@@ -0,0 +1,43 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: D:\Pooja\MSC\Pracs\Algorithm\Algorithm_Prac1_RandomizedSelection.py 
+1
+3
+4
+7
+8
+9
+>>> 
+>>> 
+>>> 
+>>> 
+===== RESTART: D:\Pooja\MSC\Pracs\Algorithm\Algorithm_Prac2_HeapSort.py =====
+Sorted array is
+5
+6
+7
+11
+12
+13
+>>> 
+>>> 
+>>> 
+>>> 
+===== RESTART: D:\Pooja\MSC\Pracs\Algorithm\Algorithm_Prac3_RadixSort.py =====
+2
+24
+45
+66
+75
+90
+170
+802
+>>> 
+>>> 
+>>> 
+>>> 
+==== RESTART: D:\Pooja\MSC\Pracs\Algorithm\Algorithm_Prac4_BucketSort.py ====
+Sorted Array is
+[0.1234, 0.3434, 0.565, 0.656, 0.665, 0.897]
+>>> 
