8f445256e90778a7060a5f50a8754a390128c4d2
pshewale <77521813+pshewale@users.noreply.github.com>
2021-03-10 18:49:33 +0530

    Add files via upload

---
+++ Algorithm_Output_5-8.py
@@ -0,0 +1,44 @@
+Python 3.7.4 (tags/v3.7.4:e09359112e, Jul  8 2019, 19:29:22) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+== RESTART: D:\Pooja\MSC\Pracs\Algorithm\Algorithm_Prac5_Folyd-Warshall.py ==
+Following matrix shows the shortest distance between every pair of vertices
+      0	
+      5	
+      8	
+      9	
+
+    INF
+      0	
+      3	
+      4	
+
+    INF
+    INF
+      0	
+      1	
+
+    INF
+    INF
+    INF
+      0	
+
+>>> 
+>>> 
+>>> 
+>>> 
+=== RESTART: D:\Pooja\MSC\Pracs\Algorithm\Algorithm_Prac6_CountingSort.py ===
+Sorted character array is eeeefggkkorss 
+>>> 
+>>> 
+>>> 
+>>> 
+=== RESTART: D:\Pooja\MSC\Pracs\Algorithm\Algorithm_Prac7_ SetCovering.py ===
+[{1, 2, 3, 8, 9, 10}, {4, 5, 7}, {5, 6, 7}]
+>>> 
+>>> 
+>>> 
+>>> 
+====== RESTART: D:\Pooja\MSC\Pracs\Algorithm\Algorithm_Prac8_Subset.py ======
+Found a subset with given sum
+>>> 
